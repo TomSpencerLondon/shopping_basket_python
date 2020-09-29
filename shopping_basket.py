6f63cdff6a817d010e45251889c60c13fdef49dc
Tom <thomas.spencer@codurance.com>
2020-09-29 21:42:58 +0100

    Add passing test for unit_price

--- shopping_basket.py
+++ shopping_basket.py
@@ -6,4 +6,6 @@ class Basket(object):
     items: List
 
     def total(self):
+        if len(self.items) > 0:
+            return self.items[0].unit_price
         return 0
--- test/shopping_basket_test.py
+++ test/shopping_basket_test.py
@@ -1,10 +1,22 @@
 import unittest
+from dataclasses import dataclass
+
 from shopping_basket import Basket
+
+@dataclass
+class Item(object):
+    unit_price: float
+    quantity: 1
+
 
 class ShoppingBasketTest(unittest.TestCase):
     def test_empty_basket_total(self):
         basket = Basket([])
         self.assertEqual(basket.total(), 0)
+
+    def test_single_item_quantity_one(self):
+        basket = Basket([Item(100.0, 1)])
+        self.assertEqual(basket.total(), 100.0)
 
 if __name__ == '__main__':
     pass
