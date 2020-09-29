02babe827d961f682b41549d937584b91e842636
Tom <thomas.spencer@codurance.com>
2020-09-29 21:36:06 +0100

    Add passing test for ShoppingBasket

--- test/shopping_basket_test.py
+++ test/shopping_basket_test.py
@@ -1,15 +1,20 @@
 import unittest
 from dataclasses import dataclass
+from typing import List
 
 
 @dataclass
 class Basket(object):
-    pass
+    items: List
+
+    def total(self):
+        return 0
 
 
 class ShoppingBasketTest(unittest.TestCase):
     def test_empty_basket_total(self):
         basket = Basket([])
+        self.assertEqual(basket.total(), 0)
 
 if __name__ == '__main__':
     pass
