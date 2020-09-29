7c26bb87b5ce82fd752d83fc649dc1b278913599
Tom <thomas.spencer@codurance.com>
2020-09-29 21:48:23 +0100

    Strongly type List with Item

--- shopping_basket.py
+++ shopping_basket.py
@@ -1,9 +1,11 @@
 from typing import List
 from dataclasses import dataclass
+from item import Item
+
 
 @dataclass
 class Basket(object):
-    items: List
+    items: List[Item]
 
     def total(self):
         if len(self.items) > 0:
