9d53e5cd04c3dc1e942ae762f351ea61a5e8a480
Tom <thomas.spencer@codurance.com>
2020-09-30 20:01:17 +0100

    Format code

--- test/shopping_basket_test.py
+++ test/shopping_basket_test.py
@@ -3,6 +3,7 @@ import unittest
 from shopping_basket import Basket
 from item import Item
 
+
 class ShoppingBasketTest(unittest.TestCase):
     def test_empty_basket_total(self):
         basket = Basket([])
@@ -19,6 +20,7 @@ class ShoppingBasketTest(unittest.TestCase):
     def test_single_item_quantity(self):
         basket = Basket([Item(100.0, 2)])
         self.assertEqual(basket.total(), 200.0)
+
 
 if __name__ == '__main__':
     pass
